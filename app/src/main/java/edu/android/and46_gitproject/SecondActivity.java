d5e4cd875087ff2bf7658ee159f0e314372e569a
Rachel Lee <ms.sseul@gmail.com>
2018-10-19 14:54:24 +0900

    SecondActivity 추가

---
+++ app/src/main/java/edu/android/and46_gitproject/SecondActivity.java
@@ -0,0 +1,13 @@
+package edu.android.and46_gitproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
