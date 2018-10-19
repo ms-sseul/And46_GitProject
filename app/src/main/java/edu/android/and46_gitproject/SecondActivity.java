4bbd620a41f0d394eaf65c1d622ffb2917973e54
Rachel Lee <ms.sseul@gmail.com>
2018-10-19 15:14:53 +0900

    SecondActivity 수정

--- app/src/main/java/edu/android/and46_gitproject/SecondActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/SecondActivity.java
@@ -2,6 +2,7 @@ package edu.android.and46_gitproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btn = findViewById(R.id.button);
     }
 }
